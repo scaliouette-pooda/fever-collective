b79ef4502060d0db4cf97076827b2fb93d268cd5
scaliouette <scaliouette@yahoo.com>
2025-11-13 21:50:36 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Events from './components/Events';
 import Contact from './components/Contact';
 import Booking from './components/Booking';
 import BookingConfirmation from './components/BookingConfirmation';
+import ClassPacks from './components/ClassPacks';
 import Registration from './components/Registration';
 import Login from './components/Login';
 import ForgotPassword from './components/ForgotPassword';
@@ -26,6 +27,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/booking/:eventId" element={<Booking />} />
           <Route path="/confirmation/:bookingId" element={<BookingConfirmation />} />
+          <Route path="/class-packs" element={<ClassPacks />} />
           <Route path="/register" element={<Registration />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
